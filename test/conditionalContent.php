dc90685f1e1f229b1cfb826bef58e4662cd58f6a
Martijn W. van der Lee <martijn@crmmailtech.nl>
2017-09-29 16:40:00 +0200

    Update to cover more situation

--- test/conditionalContent.php
+++ test/conditionalContent.php
@@ -11,12 +11,12 @@ $config = parse_ini_file('config.ini', true, INI_SCANNER_RAW);
 
 $Client = new \CAC\Component\ESP\Api\Engine\EngineApi($config);
 
-$deliveryid = $Client->createMailingFromTemplate($config['testdata']['templateid'], 'Subject: <!--[[if $fields["discount"] > 0]]-->Korting: %%discount%%<!--[[/if]]-->,  <!--[[if $fields["nodiscount"] > 0]]-->GeenKorting: %%nodiscount%%<!--[[/if]]-->.', 'John Doe', 'johndoe@example.test');
+$deliveryid = $Client->createMailingFromTemplate($config['testdata']['templateid'], 'Subject: <!--[[if $fields["discount"] > 0]]-->Discounted!<!--[[/if]]-->,  <!--[[if $fields["bonus"] > 0]]-->With &euro; %%bonus%% bonus<!--[[else]]-->No bonus<!--[[/if]]-->.', 'John Doe', 'johndoe@example.test');
 var_dump('Delivery ID', $deliveryid);
 
 $recipientid = $Client->sendMailing($deliveryid, [[
 	'email' => $config['recipient']['email'],
 	'discount' => 3,
-	'nodiscount' => 0,
+	'bonus' => 0,
 		]]);
 var_dump('Delivery recipient ID', $recipientid);
